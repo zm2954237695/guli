6af11f226461b2ae1710885158d97b67d368ca97
zm2954237695 <2954237695@qq.com>
2022-07-31 22:49:05 +0800

    7.25

--- front-end-of-grain-college-master/src/api/teacher/chapter.js
+++ front-end-of-grain-college-master/src/api/teacher/chapter.js
@@ -1,72 +1,36 @@
 import request from '@/utils/request'
-
-export default {
-
-    /**
-     * 获取所有章节小节
-     * 
-     * @param {*} courseInfoForm 
-     * @returns 
-     */
-    getAllChapterVideo(courseId) {
-        return request({
-            url: `/eduservice/edu-chapter/getChapterVideo/${courseId}`,
-            method: 'get'
-        })
-    },
-
-    /**
-     * 增加章节
-     * 
-     * @param {*} chapter 
-     * @returns 
-     */
-    addChapter(chapter) {
-        return request({
-            url: `/eduservice/edu-chapter/addChapter`,
-            method: 'post',
-            data: chapter
-        })
-    },
-
-    /**
-     * 查询章节
-     * 
-     * @param {*}} chapterId 
-     * @returns 
-     */
-    getChapterInfo(chapterId) {
-        return request({
-            url: `/eduservice/edu-chapter/getChapterInfo/${chapterId}`,
-            method: 'get'
-        })
-    },
-
-    /**
-     * 增加章节
-     * 
-     * @param {*} chapter 
-     * @returns 
-     */
-    updateChapter(chapter) {
-        return request({
-            url: `/eduservice/edu-chapter/updateChapter`,
-            method: 'post',
-            data: chapter
-        })
-    },
-
-    /**
-     * 删除章节
-     * 
-     * @param {*} chapterId 
-     */
-    removeChapter(chapterId) {
-        return request({
-            url: `/eduservice/edu-chapter/removeChapter/${chapterId}`,
-            method: 'delete'
-        })
-    }
-
-
+export default{
+  getAllChapterVideo(courseId){
+    return request({
+      url: '/eduservice/chapter/getChapterVideo/'+courseId,
+      method: 'get'
+    })
+  },
+  //添加章节
+  addChapter(chapter){
+    return request({
+      url: '/eduservice/chapter/addChapter',
+      method: 'post',
+      data: chapter
+    })
+  },
+  getChapter(chapterId){
+    return request({
+      url: '/eduservice/chapter/getChapterInfo/'+chapterId,
+      method: 'get'
+    })
+  },
+  updateChapter(chapter){
+    return request({
+      url: '/eduservice/chapter/updateChapter',
+      method: 'post',
+      data: chapter
+    })
+  },
+  deleteChapter(chapterId){
+    return request({
+      url: '/eduservice/chapter/'+chapterId,
+      method: 'delete'
+    })
+  },
 }
